7f52c36937f6074903b699f4ab7e366df10d8229
user <user@MSI>
2024-09-23 10:27:16 +0800

    2024092301

---
+++ s11215103/Models/People.cs
@@ -0,0 +1,19 @@
+﻿namespace s11215103.Models
+{
+    public class People
+    {
+        public string 帳號 { get; set; }
+        public string 性別 { get; set; }
+        public int 身高 { get; set; }
+        public float 體重 { get; set; }
+        public float 標準體重 { get; set; }
+        // 方法成員
+        public void 計算標準體重()
+        {
+            if (性別 == "男")
+                標準體重 = (身高 - 80) * 0.7f;
+            else
+                標準體重 = (身高 - 70) * 0.6f;
+        }
+    }
+}
