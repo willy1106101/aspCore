dda7645425d02a70b795c25082b8b5f1dd2b1a71
user <user@MSI>
2024-10-06 21:25:18 +0800

    2024100601

--- s11215103/Controllers/HomeController.cs
+++ s11215103/Controllers/HomeController.cs
@@ -40,5 +40,39 @@ namespace s11215103.Controllers
             return View(pList);
         }
 
+        public IActionResult Bootstrap() {
+
+            IList<People> pList =
+            [
+                new People { 帳號 = "徐嘉瑋", 性別 = "男", 身高 = 175, 體重 = 50.5f },
+                new People { 帳號 = "ysp", 性別 = "男", 身高 = 165, 體重 = 70.5f },
+                new People { 帳號 = "peter", 性別 = "女", 身高 = 165, 體重 = 70.5f },
+                new People { 帳號 = "mary", 性別 = "男", 身高 = 190, 體重 = 65.5f },
+                new People { 帳號 = "jeny", 性別 = "女", 身高 = 155, 體重 = 40.5f },
+            ];
+            foreach (var p in pList)
+            {
+                p.計算標準體重();
+                p.計算BMI();
+            }
+            return View(pList);
+        }
+
+        public IActionResult FoodList()
+        {
+
+            IList<Food> pList =
+            [
+                new Food { 編號 = "F01", 名稱 = "屯經拉麵", 單價 = 280 },
+                new Food { 編號 = "F02", 名稱 = "金三角生角片", 單價 = 540 },
+                new Food { 編號 = "F03", 名稱 = "幽靈肉粽", 單價 = 30 },
+            ];
+            return View(pList);
+        }
+
+        public IActionResult Layout()
+        {
+            return View();
+        }
     }
 }
---
+++ s11215103/Models/Food.cs
@@ -0,0 +1,9 @@
+﻿namespace s11215103.Models
+{
+    public class Food
+    {
+        public string 編號 { get; set; }
+        public string 名稱 { get; set;}
+        public int 單價 { get; set;}
+    }
+}
